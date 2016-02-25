000b24fcc127b219b34648f4afa2ac3b612f5e14
Chad Kittel <chad.kittel@gmail.com>
2016-02-24 18:44:25 -0600

    Add new metric values

--- Trek.BalihooApiClient/CampaignTacticMetric.cs
+++ Trek.BalihooApiClient/CampaignTacticMetric.cs
@@ -10,7 +10,14 @@ namespace Trek.BalihooApiClient
         public int? Sends { get; set; } // Email
         public int? Opens { get; set; } // Email
         public int? Clicks { get; set; } // Email, Paid Search
+        public int? Delivered { get; set; } // Email
+        public int? Bounced { get; set; } // Email
+        public int? Unsubscribed { get; set; } // Email
+        public int? MarkedSpam { get; set; } // Email
         public decimal? Spend { get; set; } // Paid Search, Display
-        public int? Impressions { get; set; } // Display
+        public int? Impressions { get; set; } // Paid Search, Display
+        public decimal? Ctr { get; set; } // Paid Search, Display
+        public decimal? AvgCpc { get; set; } // Paid Search, Display
+        public decimal? AvgCpm { get; set; } // Paid Search, Display
     }
 }
